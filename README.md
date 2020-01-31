d6023af0f5a727bd2a6dd2d3d00de49eb39c9cbc
Vipin <vipin.pp@xerox.nt>
2020-01-31 12:04:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-multi-module
-Spring Multi mOdule
+Spring Multi module
+
+create new module maven command
+cd erp-parent
+mvn archetype:generate -DgroupId=com.snowgol.erp  -DartifactId=#modulename#
---
+++ erp-parent/erp-main-web/src/main/java/com/snowgol/erp/sample/TestController.java
@@ -0,0 +1,14 @@
+package com.snowgol.erp.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value ="/")
+    public String onReady() {
+        return "Comming Soon...!!!";
+    }
+
+}
