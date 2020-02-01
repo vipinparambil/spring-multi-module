c6303bdb7effea55d0fe3b80e8a68550cd32db72
vipinparambil <vipinparambil@gmail.com>
2020-02-01 14:40:31 +0530

    Changed Build

--- erp-parent/erp-main-web/src/main/java/com/snowgol/erp/ErpApplication.java
+++ erp-parent/erp-main-web/src/main/java/com/snowgol/erp/ErpApplication.java
@@ -2,16 +2,22 @@ package com.snowgol.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication(
         exclude = {org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class })
-public class ErpApplication extends SpringBootServletInitializer {
+public class ErpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ErpApplication.class, args);
 
     }
-
+    
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder builder) { return
+	 * builder.sources(ErpApplication.class); }
+	 */
 }
